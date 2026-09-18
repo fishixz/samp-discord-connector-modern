60a536f0b398023fd6cfbc3fe08c7e478a401200
Fishixz <158246871+fishixz@users.noreply.github.com>
2026-09-18 14:54:02 -0300

    Generalizar interações para componentes e modais

--- src/CommandInteraction.hpp
+++ src/CommandInteraction.hpp
@@ -6,11 +6,10 @@
 #include "Callback.hpp"
 #include "Command.hpp"
 
+#include <json.hpp>
+#include <map>
 #include <string>
 #include <vector>
-
-#include <json.hpp>
-#include <boost/variant.hpp>
 
 using json = nlohmann::json;
 static CommandInteractionOption_t InvalidCommandInteractionChildOption;
@@ -27,46 +26,51 @@ class CommandInteraction
 public:
 	CommandInteraction(CommandInteractionId_t id, UserId_t user, nlohmann::json const& interaction_json);
 	~CommandInteraction() = default;
+
 	void ParseOptions(nlohmann::json const& interaction_json, std::string const& guildid);
 	void AddInteractionOption(CommandInteractionOption_t &option);
 
-	CommandInteractionId_t GetPawnId()
-	{
-		return m_ID;
-	}
+	CommandInteractionId_t GetPawnId() const { return m_ID; }
+	GuildId_t GetGuildID() const { return m_Guild; }
+	ChannelId_t GetChannelID() const { return m_Channel; }
+	UserId_t GetUserID() const { return m_InteractionUser; }
+	int GetType() const { return m_Type; }
+	int GetComponentType() const { return m_ComponentType; }
+	std::string const& GetCustomId() const { return m_CustomId; }
+	bool HasResponded() const { return m_Responded; }
 
-	GuildId_t GetGuildID()
-	{
-		return m_Guild;
-	}
+	std::vector<UserId_t> const& GetMentions() const { return m_Mentions; }
+	std::vector<CommandInteractionOption_t> const& GetOptions() const { return m_InteractionOptions; }
+	std::vector<std::string> const& GetValues() const { return m_Values; }
 
-	ChannelId_t GetChannelID()
-	{
-		return m_Channel;
-	}
-
-	std::vector<UserId_t> const& GetMentions()
-	{
-		return m_Mentions;
-	}
-
-	std::vector<CommandInteractionOption_t> const& GetOptions()
-	{
-		return m_InteractionOptions;
-	}
+	bool GetModalValue(std::string const& custom_id, std::string& value) const;
 
 	void SendEmbed(EmbedId_t embedid, const std::string message = "");
-	void SendInteractionMessage(const std::string message);
+	void SendInteractionMessage(const std::string message, bool ephemeral = false);
+	void UpdateInteractionMessage(const std::string message);
+	bool ShowModal(ModalId_t modalid);
+	bool Acknowledge();
 
 private:
+	void ParseComponentData(nlohmann::json const& interaction_json);
+	void ParseModalComponents(nlohmann::json const& components);
+	bool SendInitialResponse(int response_type, nlohmann::json const* data = nullptr);
+
 	CommandInteractionId_t m_ID;
 	Snowflake_t m_IDSnowflake;
 	Snowflake_t m_Token;
-	ChannelId_t m_Channel;
-	GuildId_t m_Guild = 0;
-	UserId_t m_InteractionUser;
+	ChannelId_t m_Channel = INVALID_CHANNEL_ID;
+	GuildId_t m_Guild = INVALID_GUILD_ID;
+	UserId_t m_InteractionUser = INVALID_USER_ID;
+	int m_Type = 0;
+	int m_ComponentType = 0;
+	std::string m_CustomId;
+	bool m_Responded = false;
+
 	std::vector<CommandInteractionOption_t> m_InteractionOptions;
 	std::vector<UserId_t> m_Mentions;
+	std::vector<std::string> m_Values;
+	std::map<std::string, std::string> m_ModalValues;
 };
 
 class CommandInteractionManager : public Singleton<CommandInteractionManager>
@@ -76,8 +80,8 @@ private:
 	CommandInteractionManager() = default;
 	~CommandInteractionManager() = default;
 
-private:
 	std::map<CommandInteractionId_t, CommandInteraction_t> m_Interactions;
+
 public:
 	CommandInteraction_t const& FindCommandInteraction(CommandInteractionId_t interaction);
 	CommandInteractionId_t AddCommandInteraction(UserId_t user, nlohmann::json const& interaction_json);
